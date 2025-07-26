265a1ee6cd1af853e97e814def42c1cbceb3c4f6
radenmasabdul <abdul.tamsis@gmail.com>
2025-07-26 21:21:54 +0700

    feat: setup endpoint

---
+++ src/utils/endpoints/sensor.js
@@ -0,0 +1,38 @@
+import api from "../services/api";
+
+export const getAllSensor = async (query) => {
+    const res = await api.get("/sensor/all", {
+        params: query,
+    });
+    return res.data;
+}
+
+export const getLocationSensor = async () => {
+    const res = await api.get("/sensor/locations");
+    return res.data;
+}
+
+export const getLocationParameter = async () => {
+    const res = await api.get("/sensor/parameters");
+    return res.data;
+}
+
+export const getSensorById = async (id) => {
+    const res = await api.get(`/sensor/${id}`);
+    return res.data;
+};
+
+export const createSensor = async (data) => {
+    const res = await api.post("/sensor/create", data);
+    return res.data;
+}
+
+export const updateSensor = async (id, data) => {
+    const res = await api.put(`/sensor/update/${id}`, data);
+    return res.data;
+}
+
+export const deleteSensor = async (id) => {
+    const res = await api.delete(`/sensor/delete/${id}`);
+    return res.data;
+}
